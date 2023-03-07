5737346ed0de278950006ce3e09457773a6c884a
javid-akthar <108605858+javid-akthar@users.noreply.github.com>
2023-03-07 23:38:46 +0530

    up

--- controller/g_man_power.js
+++ controller/g_man_power.js
@@ -1,48 +1,38 @@
-/*
-  This method calculates the remaining power
-*/
+const {inBetweenCellsLength,rqdDirFinder,directionOptions} = require('./PowerUtil');
 const gManPower = function (srcXAxis,destXAxis,srcYAxis,destYAxis,initDir) {
 
-  // if source and destination is in same axis this method will be called
   if (srcXAxis == destXAxis || srcYAxis == destYAxis) {
     return sameAxis(srcXAxis,destXAxis,srcYAxis,destYAxis,initDir);
   } else {
-    // if source and destination is in different axis this method will be called
     return differentAxis(srcXAxis,destXAxis,srcYAxis,destYAxis,initDir);
   }
 };
 
-/*
-if source and destination is in same axis this method will calculate the 
-shortest distance and the remaing power
- */
+
 function sameAxis(srcXAxis,destXAxis,srcYAxis,destYAxis,initDir) {
   const one90DegreeMove = 5;
   const totalPower = 200;
   let rqdDir = "";
-  let turnPower = 0;
   let inBetweenCells = inBetweenCellsLength(srcXAxis,destXAxis,srcYAxis,destYAxis);
   rqdDir = rqdDirFinder(srcXAxis,destXAxis,srcYAxis,destYAxis);
-  // calculating the turn power required
-  if (rqdDir == initDir) {
-    turnPower = 0;
-  } else if ((rqdDir == "E" && initDir == "W") ||(rqdDir == "W" && initDir == "E") ||
-    (rqdDir == "N" && initDir == "S") ||(rqdDir == "S" && initDir == "N")) {
-    // in this case we will take two moves to turnPower becomes 10;
-    turnPower = one90DegreeMove*2;
-  } else {
-    // in this case we will take two moves to turnPower becomes 5;
-    turnPower = one90DegreeMove;
-  }
-  // calculating the remaining power
+  turnPower = turnPowerCal(rqdDir,initDir,one90DegreeMove)
   let remainingPower = totalPower - (turnPower + inBetweenCells);
   return remainingPower;
 }
 
-/*
-if source and destination is in different axis this method will calculate 
-the shortest distance and the remaing power
- */
+function turnPowerCal(rqdDir,initDir,one90DegreeMove){
+  let turnPower = 0;
+  if (rqdDir == initDir) {
+    turnPower = 0;
+  } else if ((rqdDir == "E" && initDir == "W") ||(rqdDir == "W" && initDir == "E") ||
+    (rqdDir == "N" && initDir == "S") ||(rqdDir == "S" && initDir == "N")) {
+    turnPower = one90DegreeMove*2;
+  } else {
+    turnPower = one90DegreeMove;
+  }
+  return turnPower;
+}
+
 function differentAxis(srcXAxis,destXAxis,srcYAxis,destYAxis,initDir
 ) {
   const totalPower = 200;
@@ -50,7 +40,6 @@ function differentAxis(srcXAxis,destXAxis,srcYAxis,destYAxis,initDir
   let remainingPower = 0;
   let inBetweenCells = inBetweenCellsLength(srcXAxis,destXAxis,srcYAxis,destYAxis);
   let { option1, option2 } = directionOptions(srcXAxis,destXAxis,srcYAxis,destYAxis);
-  // calculating the remaining power
   if (initDir == option1 || initDir == option2) {
     remainingPower = totalPower - (inBetweenCells + one90DegreeMove);
   } else {
@@ -60,62 +49,4 @@ function differentAxis(srcXAxis,destXAxis,srcYAxis,destYAxis,initDir
 }
 
 
-/*
-this method computes the inbetween length between source and destination 
-excluding the turning energy spent
-*/
-function inBetweenCellsLength(srcXAxis,destXAxis,srcYAxis,destYAxis
-) {
-  const costOfTravellingPerCell = 10;
-  let dist =
-    (Math.abs(srcXAxis - destXAxis) +
-      Math.abs(srcYAxis - destYAxis)) *
-      costOfTravellingPerCell;
-  return dist;
-}
-
-/*
-if the source and destination in same axis we need to find the the direction in which the source 
-have to start to react the destination this method derives the direction in 
-which the source ahve to start
-*/
-function rqdDirFinder(srcXAxis,destXAxis,srcYAxis,destYAxis) {
-  let rqdDir = "";
-  if (srcXAxis == destXAxis) {
-    if (srcYAxis - destYAxis > 0) {
-      rqdDir = "S";
-    } else if (srcYAxis - destYAxis < 0) {
-      rqdDir = "N";
-    }
-  } else if (srcYAxis == destYAxis) {
-    if (srcXAxis - destXAxis > 0) {
-      rqdDir = "W";
-    } else if (srcXAxis - destXAxis < 0) {
-      rqdDir = "E";
-    }
-  }
-  return rqdDir;
-}
-
-/*
-if the source and destination in different axis the source may have two direction in which the source can
-start to react the destination, this method computes the two possible directions the source can start
- */
-function directionOptions(srcXAxis,destXAxis,srcYAxis,destYAxis) {
-  if (destYAxis > srcYAxis) {
-    option1 = "N";
-  } else if (destYAxis < srcYAxis) {
-    option1 = "S";
-  }
-
-  if (destXAxis > srcXAxis) {
-    option2 = "E";
-  } else {
-    option2 = "W";
-  }
-
-  return { option1,option2,};
-}
-
-
-module.exports = {inBetweenCellsLength,rqdDirFinder,directionOptions,sameAxis,differentAxis,gManPower,};
+module.exports = {sameAxis,differentAxis,gManPower};
--- test.js
+++ test.js
@@ -5,71 +5,73 @@ chai.use(deepEqualInAnyOrder);
 
 const expect = chai.expect;
 let {
-  inBetweenCellsLength,
-  rqdDirFinder,
-  directionOptions,
   sameAxis,
   differentAxis,
   gManPower,
 } = require("./controller/g_man_power");
-console.log(gManPower);
-console.log(inBetweenCellsLength);
+const {inBetweenCellsLength,rqdDirFinder,directionOptions} = require('./controller/PowerUtil');
 
+
+const  inBtwCells = [40, 100, 80, 20, 60, 60, 40, 40];
+const dirArr = ['E','W','N','S'];
+const sameAxisVal = [180,135,160,150];
+const diffAxisVal = [155,90,110,130];
+const gManPwrVal = [155,90,110,180,130,135,160,150]
 describe("Test Suite", function () {
   it("Test the inBetweenCellsLength Method", function () {
-    expect(inBetweenCellsLength(2, 4, 1, 3)).to.be.equal(40);
-    expect(inBetweenCellsLength(0, 6, 5, 1)).to.be.equal(100);
-    expect(inBetweenCellsLength(3, 1, 6, 0)).to.be.equal(80);
-    expect(inBetweenCellsLength(2, 4, 1, 1)).to.be.equal(20);
-    expect(inBetweenCellsLength(2, 4, 1, 5)).to.be.equal(60);
-    expect(inBetweenCellsLength(0, 6, 6, 6)).to.be.equal(60);
-    expect(inBetweenCellsLength(1, 5, 3, 3)).to.be.equal(40);
-    expect(inBetweenCellsLength(5, 1, 3, 3)).to.be.equal(40);
+    expect(inBetweenCellsLength(2, 4, 1, 3)).to.be.equal(inBtwCells[0]);
+    expect(inBetweenCellsLength(0, 6, 5, 1)).to.be.equal(inBtwCells[1]);
+    expect(inBetweenCellsLength(3, 1, 6, 0)).to.be.equal(inBtwCells[2]);
+    expect(inBetweenCellsLength(2, 4, 1, 1)).to.be.equal(inBtwCells[3]);
+    expect(inBetweenCellsLength(2, 4, 1, 5)).to.be.equal(inBtwCells[4]);
+    expect(inBetweenCellsLength(0, 6, 6, 6)).to.be.equal(inBtwCells[5]);
+    expect(inBetweenCellsLength(1, 5, 3, 3)).to.be.equal(inBtwCells[6]);
+    expect(inBetweenCellsLength(5, 1, 3, 3)).to.be.equal(inBtwCells[6]);
   });
   it("Test the rqdDirFinder method", function () {
-    expect(rqdDirFinder(2, 4, 1, 1)).to.be.equal("E");
-    expect(rqdDirFinder(0, 6, 6, 6)).to.be.equal("E");
-    expect(rqdDirFinder(1, 5, 3, 3)).to.be.equal("E");
-    expect(rqdDirFinder(5, 1, 3, 3)).to.be.equal("W");
+    expect(rqdDirFinder(2, 4, 1, 1)).to.be.equal(dirArr[0]);
+    expect(rqdDirFinder(0, 6, 6, 6)).to.be.equal(dirArr[0]);
+    expect(rqdDirFinder(1, 5, 3, 3)).to.be.equal(dirArr[0]);
+    expect(rqdDirFinder(5, 1, 3, 3)).to.be.equal(dirArr[1]);
   });
   it("Test the directionOptions method", function () {
     expect(directionOptions(2, 4, 1, 3)).to.deep.equalInAnyOrder({
-      option1: "N",
-      option2: "E",
+      option1: dirArr[2],
+      option2: dirArr[0],
     });
     expect(directionOptions(0, 6, 5, 1)).to.deep.equalInAnyOrder({
-      option1: "S",
-      option2: "E",
+      option1: dirArr[3],
+      option2: dirArr[0],
     });
     expect(directionOptions(3, 1, 6, 0)).to.deep.equalInAnyOrder({
-      option1: "S",
-      option2: "W",
+      option1: dirArr[3],
+      option2: dirArr[1],
     });
     expect(directionOptions(2, 4, 1, 5)).to.deep.equalInAnyOrder({
-      option1: "N",
-      option2: "E",
+      option1: dirArr[2],
+      option2: dirArr[0],
     });
   });
-  it("Test the sameAxis method", function () {
-    expect(sameAxis(2, 4, 1, 1, "E")).to.be.equal(180);
-    expect(sameAxis(0, 6, 6, 6, "S")).to.be.equal(135);
-    expect(sameAxis(1, 5, 3, 3, "E")).to.be.equal(160);
-    expect(sameAxis(5, 1, 3, 3, "E")).to.be.equal(150);
+  it("Test the sameAxis method", function () { 
+    expect(sameAxis(2, 4, 1, 1, "E")).to.be.equal(sameAxisVal[0]);
+    expect(sameAxis(0, 6, 6, 6, "S")).to.be.equal(sameAxisVal[1]);
+    expect(sameAxis(1, 5, 3, 3, "E")).to.be.equal(sameAxisVal[2]);
+    expect(sameAxis(5, 1, 3, 3, "E")).to.be.equal(sameAxisVal[3]);
   });
-  it("Test the differentAxis method", function () {
-    expect(differentAxis(2, 4, 1, 3, "E")).to.be.equal(155);
-    expect(differentAxis(0, 6, 5, 1, "W")).to.be.equal(90);
-    expect(differentAxis(3, 1, 6, 0, "N")).to.be.equal(110);
-    expect(differentAxis(2, 4, 1, 5, "S")).to.be.equal(130);
+  it("Test the differentAxis method", function () { 
+    expect(differentAxis(2, 4, 1, 3, "E")).to.be.equal(diffAxisVal[0]);
+    expect(differentAxis(0, 6, 5, 1, "W")).to.be.equal(diffAxisVal[1]);
+    expect(differentAxis(3, 1, 6, 0, "N")).to.be.equal(diffAxisVal[2]);
+    expect(differentAxis(2, 4, 1, 5, "S")).to.be.equal(diffAxisVal[3]);
   });
-  it("Test the gManPower method", function () {
-    expect(gManPower(2, 4, 1, 3, "E")).to.be.equal(155);
-    expect(gManPower(0, 6, 5, 1, "W")).to.be.equal(90);
-    expect(gManPower(3, 1, 6, 0, "N")).to.be.equal(110);
-    expect(gManPower(2, 4, 1, 1, "E")).to.be.equal(180);
-    expect(gManPower(2, 4, 1, 5, "S")).to.be.equal(130);
-    expect(gManPower(0, 6, 6, 6, "S")).to.be.equal(135);
-    expect(gManPower(1, 5, 3, 3, "E")).to.be.equal(160);
-    expect(gManPower(5, 1, 3, 3, "E")).to.be.equal(150);
+  it("Test the gManPower method", function () { 
+    expect(gManPower(2, 4, 1, 3, "E")).to.be.equal(gManPwrVal[0]);
+    expect(gManPower(0, 6, 5, 1, "W")).to.be.equal(gManPwrVal[1]);
+    expect(gManPower(3, 1, 6, 0, "N")).to.be.equal(gManPwrVal[2]);
+    expect(gManPower(2, 4, 1, 1, "E")).to.be.equal(gManPwrVal[3]);
+    expect(gManPower(2, 4, 1, 5, "S")).to.be.equal(gManPwrVal[4]);
+    expect(gManPower(0, 6, 6, 6, "S")).to.be.equal(gManPwrVal[5]);
+    expect(gManPower(1, 5, 3, 3, "E")).to.be.equal(gManPwrVal[6]);
+    expect(gManPower(5, 1, 3, 3, "E")).to.be.equal(gManPwrVal[7]);
   });
 });
